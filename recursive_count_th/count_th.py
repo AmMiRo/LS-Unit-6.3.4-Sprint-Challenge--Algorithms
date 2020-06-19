23b9faad77ba733b92c418885a69e77fcbe19699
Amos Rose <amosroseacct@gmail.com>
2020-06-19 10:31:31 -0600

    robot_wort.py complete with tests passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,15 +3,34 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word, inc=None):
-    if inc == None:
+# def count_th(word, inc=None):
+#     if inc == None:
+#         inc = 0
+#     if inc < len(word):
+#         if inc + 1 ==  len(word):
+#             return 0
+#         elif f"{word[inc]}{word[inc+1]}" == "th":
+#             return 1 + count_th(word, inc + 2)
+#         else:
+#             return 0 + count_th(word, inc + 1)
+#     else:
+#         return 0
+
+def count_th(word):
+    if "~" in word:
+        inc = word.find("~")
+    else:
         inc = 0
+        word = "~" + word
+    print(word)
     if inc < len(word):
-        if inc + 1 ==  len(word):
+        if inc + 2 ==  len(word):
             return 0
-        elif f"{word[inc]}{word[inc+1]}" == "th":
-            return 1 + count_th(word, inc + 2)
+        elif f"{word[inc + 1]}{word[inc + 2]}" == "th":
+            print(1 + count_th(word[:inc] + word[inc + 1:inc + 3] + "~" +  word[inc + 3:]))
+            return 1 + count_th(word[:inc] + word[inc + 1:inc + 3] + "~" +  word[inc + 3:])
         else:
-            return 0 + count_th(word, inc + 1)
+            print(0 + count_th(word[:inc] + word[inc + 1] + "~" +  word[inc + 2:]))
+            return 0 + count_th(word[:inc] + word[inc + 1] + "~" +  word[inc + 2:])
     else:
         return 0
